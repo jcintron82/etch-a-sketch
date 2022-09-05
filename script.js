46916977b023916c72970a94e2f6551d523843fb
Juan Cintron <j.g.cintron2@gmail.com>
2022-09-05 11:12:14 -0400

    Cleaned up code for final push

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 const container = document.getElementById('container');
 const divs = document.getElementsByClassName('divs');
-let i = [];
+const gridPrompt = document.querySelector('#gridPrompt');
 
-for (let i = 0; i < 255; i++){
+gridPrompt.addEventListener('click', function(){
+    location.reload();
+});
+
+window.onload = gridChoice();
+
+function gridChoice(){
+    for (let i = 0; i < 10000; i++){
     const div = document.createElement('div');
     div.className = 'divs';
     container.appendChild(div);
-}
+    div.addEventListener('mouseover', (event) => {
+        event.target.style.background = "salmon";
+    });
+    div.addEventListener('dblclick', (event) => {
+        event.target.style.background = "";
+    });
+}}
+
 
 function divColor(color){
         divs[i].style.background = color;
     
 }
 
-divs[i].addEventListener('mouseover', () => {
-    divColor('salmon');
-});
